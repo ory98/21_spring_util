c1556a8a24456331bc1a91a9406b97b442b06bca
User <User@F12>
2022-07-27 17:46:19 +0900

    7/27 수업

---
+++ src/main/java/com/spring/util/tiles/BootStrapController.java
@@ -0,0 +1,25 @@
+package com.spring.util.tiles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/bootstrap")
+public class BootStrapController {
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String index() {
+		return "/bootIndex";
+	}
+	
+	@RequestMapping(value="/checkout", method=RequestMethod.GET)
+	public String checkout() {
+		return "/bootCheckout";
+	}
+	
+	@RequestMapping(value="/productDetails", method=RequestMethod.GET)
+	public String productDetails() {
+		return "/product-details";
+	}
+}
